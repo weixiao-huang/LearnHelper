6181b0047969f4f6ff3856bd297654f7bf177c8b
Jiege Chen <jiegec@qq.com>
2017-10-03 23:48:20 +0800

    [WIP] Integrate TennyZhuang's thulib

--- src/api/learnhelper.js
+++
@@ -1,8 +0,0 @@
-export default class LeanrHelper {
-  constructor(user) {
-    this.user = user;
-    this.username = user.username;
-    this.password = user.getPassword();
-    this.prefix = 'https://learn.tsinghua.edu.cn';
-  }
-}
