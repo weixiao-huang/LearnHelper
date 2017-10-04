9967798abaaca1def412887e5815a86db08fa715
huangwx <hwx.simle@gmail.com>
2017-10-04 09:46:48 +0800

    Change ES5-style into ES6; Move test code into componentWillMount()

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import * as thulib from 'thulib';
 
 import {
   ContainerView,
@@ -6,25 +7,37 @@ import {
   InstructionsText,
 } from './style';
 
-user = {username:'!!USERNAME HERE!!',getPassword:()=>"!!PASSWORD HERE!!"}
+const user = {
+  username: '!!USERNAME HERE!!',
+  getPassword: () => '!!PASSWORD HERE!!',
+};
 
-var learn_helper = new thulib.LearnHelperUtil(user)
-learn_helper.login();
-var courses = learn_helper.getCourseList();
-courses.then((result) => {console.log(result)}).catch((err) => {console.log(err)})
-
-export default () => (
-  <ContainerView>
-    <WelcomeText>
-      Welcome to React Native!
-    </WelcomeText>
-    <InstructionsText>
-      To get started, edit index.ios.js
-      Edit Edit Edit
-    </InstructionsText>
-    <InstructionsText>
-      Press Cmd+R to reload,{'\n'}
-      Cmd+D or shake for dev menu
-    </InstructionsText>
-  </ContainerView>
-);
+export default class Welcome extends Component {
+  async componentWillMount() {
+    const learnHelper = new thulib.LearnHelperUtil(user);
+    learnHelper.login();
+    try {
+      const courses = await learnHelper.getCourseList();
+      console.log(courses);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  render() {
+    return (
+      <ContainerView>
+        <WelcomeText>
+        Welcome to React Native!
+        </WelcomeText>
+        <InstructionsText>
+          To get started, edit index.ios.js
+          Edit Edit Edit
+        </InstructionsText>
+        <InstructionsText>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </InstructionsText>
+      </ContainerView>
+    );
+  }
+}
