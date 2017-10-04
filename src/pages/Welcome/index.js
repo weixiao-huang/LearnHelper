ec12cd9308627494c6e265a32b4e2d53709d1ed6
Jiege Chen <jiegec@qq.com>
2017-10-04 08:47:00 +0800

    Integrate TennyZhuang's thulib; Fix ATS in Info.plist; Make Pythonic code JS-style

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -6,6 +6,13 @@ import {
   InstructionsText,
 } from './style';
 
+user = {username:'!!USERNAME HERE!!',getPassword:()=>"!!PASSWORD HERE!!"}
+
+var learn_helper = new thulib.LearnHelperUtil(user)
+learn_helper.login();
+var courses = learn_helper.getCourseList();
+courses.then((result) => {console.log(result)}).catch((err) => {console.log(err)})
+
 export default () => (
   <ContainerView>
     <WelcomeText>
@@ -13,6 +20,7 @@ export default () => (
     </WelcomeText>
     <InstructionsText>
       To get started, edit index.ios.js
+      Edit Edit Edit
     </InstructionsText>
     <InstructionsText>
       Press Cmd+R to reload,{'\n'}
